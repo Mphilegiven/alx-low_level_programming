771043392d8a7bfac5632f25ba3ec486a4ae23c3
Nkosingiphile <mphilegiven@gmail.com>
2023-04-21 07:39:10 -0700

    Push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-/** 
- * main- puttung function using push
- n
- * return: 0
+/**
+ * main - Putting function using push
+ *
+ * return : Always 0 (Success)
+ *
+ * Description: This function prints a message using the puts function
+ *
  */
-int main() {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
